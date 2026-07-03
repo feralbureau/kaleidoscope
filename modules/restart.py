7f783107a76d1fa0d8339822275503ef8cd10804
feralbureau <feralbureau@users.noreply.github.com>
2026-07-03 19:06:16 +0000

    fix wrong config call in restart

--- modules/restart.py
+++ modules/restart.py
@@ -6,6 +6,6 @@ from utils import config
 commands = ["restart", "reboot"]
 
 async def handle(app: Client, client: Client, message, args):
-    me = config.read_from_config('mainemoji')
+    me = config.read('mainemoji')
     await app.send_message(message.chat.id, f"{me} `Kaleidoscope` **is restarting...**")
     os.execv(sys.executable, ['python'] + sys.argv)
