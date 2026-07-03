af7ea1197d7f3a11f3bff34a68ae8630714a7b10
feralbureau <feralbureau@users.noreply.github.com>
2026-07-03 19:06:14 +0000

    fix wrong config call in setprefix

--- modules/setprefix.py
+++ modules/setprefix.py
@@ -6,5 +6,5 @@ commands = ["setprefix"]
 async def handle(app: Client, client: Client, message, args):
     me = config.read('mainemoji')
     new_prefix = " ".join(args)
-    config.add_to_config('prefix', new_prefix)
+    config.add('prefix', new_prefix)
     await app.send_message(message.chat.id, f"{me} **Prefix changed.** \n💫 New prefix: `({new_prefix})`")
