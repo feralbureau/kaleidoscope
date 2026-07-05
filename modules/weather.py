93afe789507921555da561896472da4b512de309
feralbureau <feralbureau@users.noreply.github.com>
2026-07-05 19:13:46 +0000

    fix city name with spaces in weather module

--- modules/weather.py
+++ modules/weather.py
@@ -5,6 +5,6 @@ commands = ["weather"]
 
 async def handle(app: Client, client: Client, message, args):
     async with python_weather.Client(unit=python_weather.METRIC) as wclient:
-        weather = await wclient.get("".join(args))
-        city = "".join(args)
+        weather = await wclient.get(" ".join(args))
+        city = " ".join(args)
         await app.send_message(message.chat.id, f"🔅 Weather in:  `{city.capitalize()}, {weather.country}` 📍\n🔱 **Kind:  **`{weather.kind}`\n🌡️ **Temperature: ** `{weather.temperature} °C`\n💨 **Feels like:  ** `{weather.feels_like} °C`")
