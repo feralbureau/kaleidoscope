035dd50107d4655a500c0a9eac6f15deb17a94bc
feralbureau <feralbureau@users.noreply.github.com>
2026-07-07 09:10:52 +0000

    fix stop module broken global and typo

--- modules/stop.py
+++ modules/stop.py
@@ -1,11 +1,11 @@
 from pyrogram import Client
 from utils import config
+from modules import ping as ping_module
 
 commands = ["stop", "exit"]
 
 async def handle(app: Client, client: Client, message, args):
-    global stop_toggle
     me = config.read('mainemoji')
-    stop_toggle = True
-    await app.send_message(message.chat.id, f"{me} `kaleidoscope` is now closed.`")
+    ping_module.stop_toggle = True
+    await app.send_message(message.chat.id, f"{me} `kaleidoscope` is now closed.")
     exit()
