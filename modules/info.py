6df52a003d0712073577b219f962d105d14d64b2
feralbureau <feralbureau@users.noreply.github.com>
2026-07-09 04:13:43 +0000

    fix cpu_percent returning zero on first call

--- modules/info.py
+++ modules/info.py
@@ -9,7 +9,7 @@ async def handle(app: Client, client: Client, message, args):
     me = config.read('mainemoji')
     ram_usage = psutil.virtual_memory().used / (1024 ** 3)
     total_ram = psutil.virtual_memory().total / (1024 ** 3)
-    cpu_usage = psutil.cpu_percent()
+    cpu_usage = psutil.cpu_percent(interval=0.1)
     os_info = f"{platform.system()} {platform.release()}"
 
     await app.send_video(message.chat.id, "assets/info.mp4", caption=f"{me} `Kaleidoscope`\n🔹 Version: `0.6.2`\n\n🌠 **Ram usage**:  `{ram_usage:.0f}GB/{total_ram:.0f}GB`\n♻ **CPU Usage**:  `{cpu_usage}%`\n💻 **OS:**  `{os_info}`")
